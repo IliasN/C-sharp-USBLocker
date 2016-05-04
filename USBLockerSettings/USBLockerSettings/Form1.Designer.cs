1be2aeb083339f7e4ea77561a038e8839f1ef39d
Ilias <iliascft@gmail.com>
2016-05-04 15:16:00 +0200

    Setting app added

---
+++ USBLockerSettings/USBLockerSettings/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace USBLockerSettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDevices = new System.Windows.Forms.ComboBox();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.btnConf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbDevices
+            // 
+            this.cmbDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDevices.FormattingEnabled = true;
+            this.cmbDevices.Location = new System.Drawing.Point(15, 25);
+            this.cmbDevices.Name = "cmbDevices";
+            this.cmbDevices.Size = new System.Drawing.Size(260, 21);
+            this.cmbDevices.TabIndex = 0;
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Location = new System.Drawing.Point(12, 9);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(121, 13);
+            this.lblSelect.TabIndex = 1;
+            this.lblSelect.Text = "Select your key device :";
+            // 
+            // btnConf
+            // 
+            this.btnConf.Location = new System.Drawing.Point(15, 52);
+            this.btnConf.Name = "btnConf";
+            this.btnConf.Size = new System.Drawing.Size(260, 40);
+            this.btnConf.TabIndex = 2;
+            this.btnConf.Text = "Configure";
+            this.btnConf.UseVisualStyleBackColor = true;
+            this.btnConf.Click += new System.EventHandler(this.btnConf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 113);
+            this.Controls.Add(this.btnConf);
+            this.Controls.Add(this.lblSelect);
+            this.Controls.Add(this.cmbDevices);
+            this.Name = "Form1";
+            this.Text = "USB Locker Settings";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDevices;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.Button btnConf;
+    }
+}
+
---
+++ USBLockerSettings/USBLockerSettings/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USBLockerSettings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDevices.Items.Clear();
+            DriveInfo[] driveList = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in driveList)
+            {
+                cmbDevices.Items.Add(drive.Name.ToString());
+            }
+            cmbDevices.SelectedIndex = 0;
+
+        }
+
+        private void btnConf_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SHA1 encrypt = new SHA1CryptoServiceProvider();
+                string newCode = BitConverter.ToString(encrypt.ComputeHash(Encoding.UTF8.GetBytes(DateTime.Now.ToString())));
+
+                File.WriteAllText("code.txt", newCode);
+                File.WriteAllText(cmbDevices.SelectedItem + "usblock.txt", newCode);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR : " + ex.ToString(),"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                Form1_Load(this,e);
+            }
+        }
+    }
+}
